f90fda3ade3dd943379c16e528d88b0e8bb6f1ec
hmalkan87 <hmalkan@gmail.com>
2020-03-13 21:16:37 +0300

    Context object created (NorthwindContext)

---
+++ KaraCadir/KaraCadir.Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,19 @@
+﻿using Karacadir.Northwind.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaraCadir.Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-PJQC6VC\SQLEXPRESS; Database=Northwind; Integrated Security=True;");
+        }
+
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
