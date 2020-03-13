16f2635e174b582d05be9906671dca1950dd8f01
hmalkan87 <hmalkan@gmail.com>
2020-03-14 00:07:41 +0300

    Manager classes created (ProductManager, CategoryManager)

---
+++ KaraCadir/KaraCadir.Northwind.Business/Concrete/CategoryManager.cs
@@ -0,0 +1,39 @@
+﻿using Karacadir.Northwind.Entities.Concrete;
+using KaraCadir.Northwind.Business.Abstract;
+using KaraCadir.Northwind.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaraCadir.Northwind.Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private ICategoryDal _categoryDal;
+        
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetList();
+        }
+
+        public void Add(Category category)
+        {
+            _categoryDal.Add(category);
+        }
+
+        public void Update(Category category)
+        {
+            _categoryDal.Update(category);
+        }
+
+        public void Delete(int categoryId)
+        {
+            _categoryDal.Delete(categoryId);
+        }
+    }
+}
