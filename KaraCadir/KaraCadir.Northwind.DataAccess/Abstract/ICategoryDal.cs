9c76f7b4f22a8d6441701a3322578f08c57943d8
hmalkan87 <hmalkan@gmail.com>
2020-03-13 18:14:31 +0300

    Data access interfaces created (ICategoryDal, IProductDal)

---
+++ KaraCadir/KaraCadir.Northwind.DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,10 @@
+﻿using Karacadir.Northwind.Entities.Concrete;
+using KaraCadir.Core.DataAccess;
+
+namespace KaraCadir.Northwind.DataAccess.Abstract
+{
+    public interface ICategoryDal : IEntityRepository<Category>
+    {
+        //Custom operations
+    }
+}
---
+++ KaraCadir/KaraCadir.Northwind.DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using Karacadir.Northwind.Entities.Concrete;
+using KaraCadir.Core.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KaraCadir.Northwind.DataAccess.Abstract
+{
+    public interface IProductDal : IEntityRepository<Product>
+    {
+        //Custom operations
+    }
+}
