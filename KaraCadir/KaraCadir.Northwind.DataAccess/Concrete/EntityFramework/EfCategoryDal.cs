75e8177036564b6882849a848ec950916aeb034f
hmalkan87 <hmalkan@gmail.com>
2020-03-13 18:56:18 +0300

    Data access concrete classes created (EfCategoryDal, EfProductDal)

---
+++ KaraCadir/KaraCadir.Northwind.DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,11 @@
+﻿using Karacadir.Northwind.Entities.Concrete;
+using KaraCadir.Core.DataAccess.EntityFramework;
+using KaraCadir.Northwind.DataAccess.Abstract;
+
+namespace KaraCadir.Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class EfCategoryDal : EfEntityRepositoryBase<Category, NorthwindContext>, ICategoryDal
+    {
+
+    }
+}
---
+++ KaraCadir/KaraCadir.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Karacadir.Northwind.Entities.Concrete;
+using KaraCadir.Core.DataAccess.EntityFramework;
+using KaraCadir.Northwind.DataAccess.Abstract;
+
+namespace KaraCadir.Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    {
+
+    }
+}
