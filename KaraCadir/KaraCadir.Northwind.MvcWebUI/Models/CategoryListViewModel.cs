4abd9e3c01f72644fbdff13dc8fef93984d6be27
hmalkan87 <hmalkan@gmail.com>
2020-03-18 22:20:47 +0300

    Pagination infrastructure, and listing products by category have implemented

--- KaraCadir/KaraCadir.Northwind.MvcWebUI/Models/CategoryListViewModel.cs
+++ KaraCadir/KaraCadir.Northwind.MvcWebUI/Models/CategoryListViewModel.cs
@@ -9,5 +9,6 @@ namespace KaraCadir.Northwind.MvcWebUI.Models
     public class CategoryListViewModel
     {
         public List<Category> Categories { get; set; }
+        public int CurrentCategory { get; set; }
     }
 }
