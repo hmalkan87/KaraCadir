0f64f595522fb66ab78691cd4fbc3e32632d5131
hmalkan87 <hmalkan@gmail.com>
2020-03-24 21:32:09 +0300

    Client-side validation has been intagrated

--- KaraCadir/Karacadir.Northwind.Entities/Concrete/ShippingDetails.cs
+++ KaraCadir/Karacadir.Northwind.Entities/Concrete/ShippingDetails.cs
@@ -7,16 +7,18 @@ namespace Karacadir.Northwind.Entities.Concrete
 {
     public class ShippingDetails
     {
-        [Required]
+        //[Required(ErrorMessage = "* Ad bilgisi girmediniz!")]
         public string FirstName { get; set; }
-        [Required]
+        //[Required(ErrorMessage = "* Soyad bilgisi girmediniz!")]
         public string LastName { get; set; }
-        [Required]
+        //[Required(ErrorMessage = "* E-posta bilgisi girmediniz!")]
         [DataType(DataType.EmailAddress)]
+        //[MinLength(5)]
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         public string Email { get; set; }
-        [Required]
+        //[Required(ErrorMessage = "* Şehir bilgisi girmediniz!")]
         public string City { get; set; }
-        [Required]
+        //[Required(ErrorMessage = "* Adres bilgisi girmediniz!")]
         public string Address { get; set; }
         //[Required]
         //[Range(18, 150)]
